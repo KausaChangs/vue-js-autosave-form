ab60218b0fcde552dc4e81cc8458aa476d031d09
KausaChangs <changayakausa@gmail.com>
2022-09-25 12:52:06 +0200

    still has a lot of fixing needed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div id="app">
+    <h3>FireBase Data</h3>
+    {{ firebaseData }}
+  </div>
 </template>
 
 <script>
 import { db } from "./firebase";
 
-export default {};
+const documentPath = "contacts/kausa";
+
+export default {
+  data() {
+    return {
+      firebaseData: null,
+    };
+  },
+
+  firestore() {
+    return {
+      firebaseData: db.doc(documentPath),
+    };
+  },
+};
 </script>
